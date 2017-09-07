8d6a90cc76d17297f6d4fd72395a3ae84c5960ff
gora-geo <safonov-georgiy@mail.ru>
2017-09-07 10:40:52 +0400

    first commit

---
+++ landing/urls.py
@@ -0,0 +1,19 @@
+# from django.conf.urls import url, include
+# from django.contrib import admin
+# from landing import views
+#
+#
+#
+# urlpatterns=[
+#
+#     url(r'^$', views.home, name='home'),
+#     url(r'^landing123/$', views.landing, name='landing'),
+# ]
+from django.conf.urls import url, include
+from django.contrib import admin
+from landing import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^landing123/$', views.landing, name='landing'),
+]
